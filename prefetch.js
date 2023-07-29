821a28a503affc058809e201ea98256e92590285
Hacker <36737369+lxhguard@users.noreply.github.com>
2023-07-30 01:47:28 +0800

    Update prefetch.js

--- prefetch.js
+++ prefetch.js
@@ -12,6 +12,7 @@ window.onload=function(){
         const lastNum = str.charAt(str.length - 1);
         if (['3', '5'].includes(lastNum)) {
             console.log('性能-');
+            alert('性能-');
             erweimaDom.setAttribute("src", 'https://article.biliimg.com/bfs/article/61923eb9defad1e68b7e43e87e4355e1e2a5c47c.png');   
         }
     } catch(e) {
